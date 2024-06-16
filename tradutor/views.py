cd5bcaf0f238e64a71fbfe3127198e318b188453
CaioDuart-dev <caioduarte330@gmail.com>
2024-06-15 21:48:30 -0300

    first commit

---
+++ tradutor/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from deep_translator import GoogleTranslator # type: ignore
+# from 
+# Create your views here.
+
+def index(request):
+    return render(request,'tradutor/index.html' )
+
+def translate(request):
+    language1 = request.POST.get('languages1')
+    language2 = request.POST.get('languages2')
+    text = request.POST.get('text')
+    traduction = request.POST.get('traduction')
+
+    translator = GoogleTranslator(source=language1,target=language2)
+    traduction = translator.translate(text)
+    tam1 = len(language1)
+    tam2 = len(language2)
+
+    context = {
+        'traduction' : traduction,
+        'text' : text,
+        'language1' : language1,
+        'language2' : language2,
+        'tam1' : tam1,
+        'tam2' : tam2
+        }
+    return render(request,'tradutor/index.html', context)
+
+
