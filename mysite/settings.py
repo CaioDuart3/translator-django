e8395da92f86a905ff4d768109e0e08d1042ead3
CaioDuart-dev <caioduarte330@gmail.com>
2024-06-15 22:40:50 -0300

    update for deploy

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-$kojs33&rxe&w^2jo)bucj6pu!qgyz=g3z#@b1(iu%=$t3tl7+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
