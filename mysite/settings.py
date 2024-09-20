4f9e4fbd7b15fddeac8a2eae1a5c32fb03d059ba
Caio Duarte <caioduarte330@gmail.com>
2024-09-20 20:29:25 -0300

    simplifying code

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 import os
 from pathlib import Path
+from decouple import config 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$kojs33&rxe&w^2jo)bucj6pu!qgyz=g3z#@b1(iu%=$t3tl7+'
+SECRET_KEY = config('SECRE_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- tradutor/views.py
+++ tradutor/views.py
@@ -1,31 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from deep_translator import GoogleTranslator # type: ignore
+from deep_translator import GoogleTranslator
 # from 
 # Create your views here.
 
 def index(request):
+    if request.method == "POST":
+        language1 = request.POST.get('languages1')
+        language2 = request.POST.get('languages2')
+        text = request.POST.get('text')
+        traduction = request.POST.get('traduction')
+
+        translator = GoogleTranslator(source=language1,target=language2)
+        traduction = translator.translate(text)
+        tam1 = len(language1)
+        tam2 = len(language2)
+
+        context = {
+            'traduction' : traduction,
+            'text' : text,
+            'language1' : language1,
+            'language2' : language2,
+            'tam1' : tam1,
+            'tam2' : tam2
+            }
+        return render(request,'tradutor/index.html', context)
     return render(request,'tradutor/index.html' )
-
-def translate(request):
-    language1 = request.POST.get('languages1')
-    language2 = request.POST.get('languages2')
-    text = request.POST.get('text')
-    traduction = request.POST.get('traduction')
-
-    translator = GoogleTranslator(source=language1,target=language2)
-    traduction = translator.translate(text)
-    tam1 = len(language1)
-    tam2 = len(language2)
-
-    context = {
-        'traduction' : traduction,
-        'text' : text,
-        'language1' : language1,
-        'language2' : language2,
-        'tam1' : tam1,
-        'tam2' : tam2
-        }
-    return render(request,'tradutor/index.html', context)
-
 
